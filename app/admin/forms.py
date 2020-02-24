da66af691bf3d0a3a8cde122498cef211e60a7af
Giacomo Caironi <giacomo.caironi@gmail.com>
2020-02-24 18:24:02 +0100

    user_detail admin view added

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -4,5 +4,10 @@ from wtforms.validators import *
 from app.admin.models import *
 
 
-# class Form(FlaskForm):
-#     pass
+class UserDetail(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password_hash = StringField("Password", validators=[DataRequired(), Length(min=6)])
+    is_admin = BooleanField("admin")
+    is_confirmed = BooleanField("confirmed")
+    submit = SubmitField("Save")
--- app/admin/views.py
+++ app/admin/views.py
@@ -6,3 +6,23 @@ from app.auth import confirmation_required, admin_required
 from app.admin import admin_blueprint
 from app.admin.models import *
 from app.admin.forms import *
+
+from app.auth.models import User
+
+
+@admin_blueprint.route("/")
+@admin_required
+def admin_index():
+    return render_template("custom_admin/index.html")
+
+
+@admin_blueprint.route("/user/<user_id>", methods=["GET", "POST"])
+@admin_required
+def user_detail(user_id):
+    user = User.query.filter_by(id=user_id).first_or_404()
+    form = UserDetail(obj=user)
+    if form.validate_on_submit():
+        form.populate_obj(user)
+        db.session.commit()
+        return redirect(url_for("custom_admin.user_detail", user_id=user_id))
+    return render_template("custom_admin/user_detail.html", form=form)
